cfb9fd7cabe196d610730da75675523daf7f9a15
Endrit Gjokaj <70038985+endritgjokaj@users.noreply.github.com>
2023-04-22 00:45:16 +0200

    Shtimi i tabelave ne db

--- knk_projekti.sql
+++ knk_projekti.sql
@@ -1,8 +1,9 @@
 create database menaxhimi_i_fluturimeve_airoport;
 
 use menaxhimi_i_fluturimeve_airoport;
+
 CREATE TABLE perdoruesit (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   emri VARCHAR(255),
   mbiemri VARCHAR(255),
   email VARCHAR(255),
@@ -10,22 +11,54 @@ CREATE TABLE perdoruesit (
   mosha INT,
   gjinia VARCHAR(10),
   adresa VARCHAR(255),
-  numri_telefonit VARCHAR(20)
+  numri_telefonit VARCHAR(20),
+  admin boolean
+);
+
+
+CREATE TABLE pasagjeret (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  perdoruesi_id INT,
+  emri VARCHAR(255),
+  mbiemri VARCHAR(255),
+  email VARCHAR(255),
+  mosha INT,
+  adresa VARCHAR(255),
+  numri_telefonit VARCHAR(20),
+  nacionaliteti VARCHAR(40),
+  FOREIGN KEY (perdoruesi_id) REFERENCES perdoruesit(id)
+);
+
+CREATE TABLE rezervimet(
+   id INT PRIMARY KEY AUTO_INCREMENT,
+   pasagjeri_id INT,
+   fluturimi_id INT,
+   aeroplani_id INT,
+   numri_uleses INT,
+   FOREIGN KEY (pasagjeri_id) REFERENCES pasagjeret(id),
+   FOREIGN KEY (fluturimi_id) REFERENCES fluturimet(id),
+   FOREIGN KEY (aeroplani_id) REFERENCES aeroplanet(id)
 );
 
 CREATE TABLE fluturimet (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   airline VARCHAR(50),
   qyteti_nisjes VARCHAR(50),
-  data_nisjes DATETIME,
+  nisja TIMESTAMP,  # data dhe ora e nisjes
   qyteti_arritjes VARCHAR(50),
-  data_arritjes DATETIME,
+  arritja TIMESTAMP, # data dhe ora e arritjes
   status VARCHAR(20)
+);
+
+CREATE TABLE aeroplanet(
+   id INT PRIMARY KEY AUTO_INCREMENT,
+   kompania VARCHAR(150),
+   kapaciteti INT,
+   tipi VARCHAR(50)
 );
 
 CREATE TABLE bileta(
    id INT PRIMARY KEY,
-   dy_drejtime boolean
-   
+   çmimi INT
 );
 
