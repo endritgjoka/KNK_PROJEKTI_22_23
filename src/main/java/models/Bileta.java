b9b1323e2c4c729090d77551d9927f5b7de2bf27
Fisnik Hazrolli <71229274+fisnikhz@users.noreply.github.com>
2023-04-30 22:59:22 +0200

    Klasa Bileta

---
+++ src/main/java/models/Bileta.java
@@ -0,0 +1,22 @@
+package models;
+
+public class Bileta {
+    private int çmimi;
+
+    public  Bileta(){
+
+    }
+    public Bileta(int çmimi) {
+        this.çmimi = çmimi;
+    }
+
+    public int getÇmimi() {
+        return çmimi;
+    }
+
+    public void setÇmimi(int çmimi) {
+        this.çmimi = çmimi;
+    }
+
+
+}
