94fcbae8ad9684724f2c7a3a460c6cedef819eca
AlbaThaqi <alba.thaqi@student.uni-pr.edu>
2023-05-01 15:05:56 +0200

    After login go to the homepage

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -6,10 +6,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Paths;
 
 public class LoginController {
 
@@ -18,13 +21,29 @@ public class LoginController {
     @FXML
     private TextField password;
     @FXML
+    private Button submit;
+    @FXML
     private void loginClick(ActionEvent event){
         System.out.println("Login button clicked!");
         String username = this.username.getText();
         String password = this.password.getText();
-        System.out.printf("Username: %s, Password: %s \n", username,password);
-    };
+//        System.out.printf("Username: %s, Password: %s \n", username,password);
 
+        if (event.getSource() == submit) {
+
+            try {
+                Node node = (Node) event.getSource();
+                Stage stage = (Stage) node.getScene().getWindow();
+                stage.close();
+                Scene scene = new Scene(FXMLLoader.load(getClass().getResource("home.fxml")));
+                stage.setScene(scene);
+                stage.show();
+
+            } catch (IOException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
+    };
     @FXML
     private void goToSignUp(ActionEvent event) throws IOException {
         Parent parenti = FXMLLoader.load(getClass().getResource("signup.fxml"));
