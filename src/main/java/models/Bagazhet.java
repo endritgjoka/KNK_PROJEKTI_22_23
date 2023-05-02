c50d5c23f3cb04efa8cdafc4d5a0280f99d120b1
Veronakabashi1 <116003282+Veronakabashi1@users.noreply.github.com>
2023-05-02 16:34:26 +0200

    Bagazhet_

--- src/main/java/models/Bagazhet.java
+++ src/main/java/models/Bagazhet.java
@@ -1,4 +1,18 @@
 package models;
 
 public class Bagazhet {
+    private int id;
+    private  int pasagjeri_id;
+    private int numri_bagazhit;
+    private double pesha_bagazhit;
+
+    public Bagazhet(){
+
+    }
+    public Bagazhet(int id,int pasagjeri_id,int numri_bagazhit, double pesha_bagazhit){
+        this.id=id;
+        this.pasagjeri_id=pasagjeri_id;
+        this.numri_bagazhit=numri_bagazhit;
+        this.pesha_bagazhit=pesha_bagazhit;
+    }
 }
