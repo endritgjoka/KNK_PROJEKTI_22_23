421f9e6e3aa9109484b8b753ebe8574e2d78b869
Veronakabashi1 <116003282+Veronakabashi1@users.noreply.github.com>
2023-05-02 16:43:34 +0200

    Bagazhet(set,get metoda)

--- src/main/java/models/Bagazhet.java
+++ src/main/java/models/Bagazhet.java
@@ -15,4 +15,28 @@ public class Bagazhet {
         this.numri_bagazhit=numri_bagazhit;
         this.pesha_bagazhit=pesha_bagazhit;
     }
+    public int getId(){
+        return id;
+    }
+    public int getPasagjeri_Id(){
+        return pasagjeri_id;
+    }
+    public int getNumri_bagazhit() {
+        return numri_bagazhit;
+    }
+    public double getPesha_bagazhit(){
+        return pesha_bagazhit;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public void setPasagjeri_Id(int pasagjeri_id){
+        this.pasagjeri_id=pasagjeri_id;
+    }
+    public void setNumri_bagazhit(int numri_bagazhit){
+        this.numri_bagazhit=numri_bagazhit;
+    }
+    public void setPesha_bagazhit(double pesha_bagazhit){
+        this.pesha_bagazhit=pesha_bagazhit;
+    }
 }
