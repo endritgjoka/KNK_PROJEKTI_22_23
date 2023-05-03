edcced9268949d3e8bdf9ecb191a72b724de328b
Fisnik Hazrolli <71229274+fisnikhz@users.noreply.github.com>
2023-05-03 10:54:44 +0200

    krijimi i fxml per pagesa dhe krijimi i kontrollerit PagesaController

---
+++ src/main/java/controllers/PagesaController.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+
+public class PagesaController {
+
+    @FXML
+    private TextField cardNameField;
+
+    @FXML
+    private TextField cardNumberField;
+
+    @FXML
+    private RadioButton creditCardRadio;
+
+    @FXML
+    private TextField cvvField;
+
+    @FXML
+    private RadioButton debitCardRadio;
+
+    @FXML
+    private TextField expirationDateField;
+
+    @FXML
+    private ToggleGroup pagesa;
+
+    @FXML
+    private RadioButton paypalRadio;
+
+    @FXML
+    private BorderPane root;
+
+}
