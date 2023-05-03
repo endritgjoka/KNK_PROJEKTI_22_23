2ace29cac9ed6a20d9d306e0f581dc5245428609
erblingerbeshi <erblin.gerbeshi2019@gmail.com>
2023-05-03 15:20:04 +0200

    goToLogin HomeController

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -6,14 +6,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Paths;
 
 public class HomeController {
 
 
+    @FXML
+    private Hyperlink login;
     @FXML
     private void help(ActionEvent event) throws Exception {
 
@@ -23,5 +27,16 @@ public class HomeController {
         primaryStage.setScene(scene);
         primaryStage.show();
 
+    }
+
+    @FXML
+    public void goToLogin(ActionEvent event) throws IOException {
+
+        Parent parenti = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(parenti);
+        Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        login.setVisible(false);
     }
 }
