ec958039e2d7ad95208c614cd13320a7d7eafe36
Fisnik Hazrolli <71229274+fisnikhz@users.noreply.github.com>
2023-05-04 11:59:58 +0200

    krijimi i fxml per caktimin e fluturimit dhe krijimi i kontrollerit & logo update

---
+++ src/main/java/controllers/FromToController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+
+public class FromToController {
+    @FXML
+    private ChoiceBox prej;
+    @FXML
+    private ChoiceBox te;
+    @FXML
+    private DatePicker data_nisjes;
+    @FXML
+    private DatePicker data_kthimit;
+}
