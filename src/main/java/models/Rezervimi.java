4528d1664b46ec0e37215f4ff236647e271ca60d
alnitakabashi <118002760+alnitakabashi@users.noreply.github.com>
2023-05-07 00:01:28 +0200

    Konstruktorii

--- src/main/java/models/Rezervimi.java
+++ src/main/java/models/Rezervimi.java
@@ -11,5 +11,12 @@ public class Rezervimi {
 
    }
 
+   public Rezervimi(int id,int perdoruesi_id, String adresa,String nacionaliteti, String numri_telefonit ){
+       this.id=id;
+       this.perdoruesi_id=perdoruesi_id;
+       this.adresa=adresa;
+       this.nacionaliteti=nacionaliteti;
+       this.numri_telefonit=numri_telefonit;
+   }
 
 }
