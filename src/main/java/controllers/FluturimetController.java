f0fb029f1310921e718d28d046149dc01908edb2
AlbaThaqi <alba.thaqi@student.uni-pr.edu>
2023-05-08 16:57:38 +0200

    Reservations view update

--- src/main/java/controllers/FluturimetController.java
+++ src/main/java/controllers/FluturimetController.java
@@ -1,9 +1,26 @@
 package controllers;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class FluturimetController {
 
     public void btnFilter(ActionEvent actionEvent) {
+    }
+    
+    @FXML
+    private void shkoMbrapa(ActionEvent event) throws IOException {
+        Parent parenti = FXMLLoader.load(getClass().getResource("home.fxml"));
+        Scene scene = new Scene(parenti);
+        Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
